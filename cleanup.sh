1f48d8a59aad50b908bc3dff216fdba84cc7d6b8
xaizone <git@xaizone.eu>
2023-02-27 10:08:21 +0100

    fix: rename in script

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 find /home/david/www/xai.zone -type f -not \
     -name 'index.php' -not \
     -name 'robots.txt' -not \
-    -name 'info.txt' -not \
+    -name 'readme' -not \
     -name 'cleanup.sh' -not \
     -path '/home/david/www/xai.zone/.*' \
     -mtime +14 | xargs rm
