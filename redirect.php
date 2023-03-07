86c3b8ce5238d467e225005d2b6f781c8e6ae865
xaizone <git@xaizone.eu>
2023-03-07 18:12:29 +0100

    feat: 404 instead of redirect

--- redirect.php
+++ redirect.php
@@ -4,7 +4,8 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['id'])) {
     if (file_exists(urlFolder."/".$_GET['id'])) {
 		header("Location: ".file_get_contents(urlFolder."/".$_GET['id']));
     } else {
-		header("Location: ".(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? "https" : "http").'://'.$_SERVER['HTTP_HOST']);
+		http_response_code(404);
+        echo "<pre>404 not found</pre>";
     }
 }
 ?>
