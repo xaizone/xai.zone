1f6d9dc19921d0e97a32cd37557aa169d57bb0c8
xaizone <git@xaizone.eu>
2023-03-08 21:11:26 +0100

    style: indenting

--- redirect.php
+++ redirect.php
@@ -2,9 +2,9 @@
 include 'config.php';
 if($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['id'])) {
     if (file_exists(urlFolder."/".$_GET['id'])) {
-		header("Location: ".file_get_contents(urlFolder."/".$_GET['id']));
+        header("Location: ".file_get_contents(urlFolder."/".$_GET['id']));
     } else {
-		http_response_code(404);
+        http_response_code(404);
         echo "<pre>404 not found</pre>";
     }
 }
